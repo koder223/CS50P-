e291ee455bd75379991741fa511d3a772aadfe18
koder223 <119615233+koder223@users.noreply.github.com>
2023-02-01 11:50:04 +0530

    Add files via upload

---
+++ indoorvoice.py
@@ -0,0 +1,13 @@
+# WRITING IN ALL CAPS IS LIKE YELLING.
+# Best to use your “indoor voice” sometimes, writing entirely in lowercase.
+# In a file called indoorvoice.py, implement a program in Python that prompts the user for input and
+# then outputs that same input in lowercase. Punctuation and whitespace should be outputted unchanged.
+# You’re welcome, but not required, to prompt the user explicitly,
+# as by passing a str of your own as an argument to input.
+
+# INDOOR VOICE
+
+# Taking the input
+indoor_voice = input("")
+# Converting any upper case letter(s) into lower case using variablename.lower()
+print(indoor_voice.lower())
